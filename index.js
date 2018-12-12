76840214e71c2eb1b8e55bdef42b1f7dad174da7
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-12-11 22:55:41 -0300

    Permitindo no express o uso do json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const app = express();
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/producthunt', { useNewUrlParser: true });
 
