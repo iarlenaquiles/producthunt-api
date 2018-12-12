82b64e79ca641013c1dd8d933f4dd9a60c5eee85
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-12-11 22:21:21 -0300

    Conectando com o mongo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/producthunt', { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
     res.send('Hello api');
