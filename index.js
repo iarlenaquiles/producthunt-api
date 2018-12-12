e4c4bf20656ede1b1d6a2183ca1c5f7aa519d45d
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-12-11 22:30:35 -0300

    Chamando o require dir para o model do produto

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/producthunt', { useNewUrlParser: true });
+
+requireDir('./models');
 
 app.get('/', (req, res) => {
     res.send('Hello api');
